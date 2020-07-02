b9f9a80620810ec903c2c8af8919127f769f0a6b
Eugeny Kurkovich <stunko@scalr.com>
2020-07-02 14:41:12 +0300

    fix

--- sc16085/main.tf
+++ sc16085/main.tf
@@ -16,13 +16,13 @@ resource scalr_workspace test {
 #  organization = "org-sgpvfvrkj5ao2j0"
 #}
 
-output workspace_user_email {
-  value = "${data.scalr_workspace.test.created_by.0.email}"
-}
+#output workspace_user_email {
+#  value = "${data.scalr_workspace.test.created_by.0.email}"
+#}
 
-output workspace_user_name {
-  value = "${data.scalr_workspace.test.created_by.0.username}"
-}
-output workspace_full_name {
-  value = "${data.scalr_workspace.test.created_by.0.full_name}"
-}
+#output workspace_user_name {
+#  value = "${data.scalr_workspace.test.created_by.0.username}"
+#}
+#output workspace_full_name {
+#  value = "${data.scalr_workspace.test.created_by.0.full_name}"
+#}
