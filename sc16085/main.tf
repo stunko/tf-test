5075ff54e06b1b0bb226a793ab879860882878af
Eugeny Kurkovich <stunko@scalr.com>
2020-07-02 14:44:29 +0300

    fix

--- sc16085/main.tf
+++ sc16085/main.tf
@@ -5,24 +5,24 @@ provider "scalr" {
 }
 
 resource scalr_workspace test {
-  name              = "stunko2"
+  name              = "stunko3"
   organization      = "org-sgpvfvrkj5ao2j0"
   auto_apply        = "true"
   terraform_version = "0.12.19"
 }
 
-#data scalr_workspace test {
-#  name         = "${scalr_workspace.test.name}"
-#  organization = "org-sgpvfvrkj5ao2j0"
-#}
+data scalr_workspace test {
+  name         = "${scalr_workspace.test.name}"
+  organization = "org-sgpvfvrkj5ao2j0"
+}
 
-#output workspace_user_email {
-#  value = "${data.scalr_workspace.test.created_by.0.email}"
-#}
+output workspace_user_email {
+  value = "${scalr_workspace.test.created_by.0.email}"
+}
 
-#output workspace_user_name {
-#  value = "${data.scalr_workspace.test.created_by.0.username}"
-#}
-#output workspace_full_name {
-#  value = "${data.scalr_workspace.test.created_by.0.full_name}"
-#}
+output workspace_user_name {
+  value = "${scalr_workspace.test.created_by.0.username}"
+}
+output workspace_full_name {
+  value = "${scalr_workspace.test.created_by.0.full_name}"
+}
