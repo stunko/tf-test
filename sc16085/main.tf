c5a101348a11412c74fbda529581ad963463edad
Eugeny Kurkovich <stunko@scalr.com>
2020-07-02 13:37:28 +0300

    fix

--- sc16085/main.tf
+++ sc16085/main.tf
@@ -1,9 +1,11 @@
 
 provider "scalr" {
+  hostname = "var.hostname"
+  token    = "var.api_token"
 }
 
 resource scalr_workspace test {
-  name              = "stunko2"
+  name              = "stunko"
   organization      = "org-sgpvfvrkj5ao2j0"
   auto_apply        = "true"
   terraform_version = "0.12.19"
