f0f8180e8d58ff973f03fa2c741901641d68df46
Eugeny Kurkovich <stunko@scalr.com>
2020-07-10 17:28:30 +0300

    Fix root config

--- random_string/variables.tf
+++ random_string/variables.tf
@@ -1,11 +1,11 @@
 variable "length" {
   description = "The length of random string"
-  default = 20
+  default = 40
 }
 
 variable "min_upper" {
   description = "Minimum number of uppercase alphabet characters in random string"
-  default = 6
+  default = 16
 }
 
 variable "min_numeric" {
